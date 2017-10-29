9e5ad02c2b2c9507869924a2bb744cf839f2d244
Mariusz Kotas <m@mkotas.cz>
2017-10-29 14:52:30 +0100

    Update running scripts

--- rungame_same.bat
+++ rungame_same.bat
@@ -2,4 +2,4 @@ del *.log
 del *.hlt
 
 dotnet build src\BotMarfu.csproj -o ..\artefacts\marfu
-halite.exe -d "240 160" "dotnet artefacts\marfu\BotMarfu.dll" "dotnet artefacts\marfu\BotMarfu.dll"
+halite.exe -d "240 160" -s 33 "dotnet artefacts\marfu\BotMarfu.dll" "dotnet artefacts\marfu\BotMarfu.dll"
--- rungame_v2.bat
+++ rungame_v2.bat
@@ -2,4 +2,4 @@ del *.log
 del *.hlt
 
 dotnet build src\BotMarfu.csproj -o ..\artefacts\marfu
-halite.exe -d "240 160" "dotnet artefacts\marfu\BotMarfu.dll" "dotnet artefacts\opponent_v2\BotMarfu_old.dll" -t
+halite.exe -d "240 160" -s 33 "dotnet artefacts\marfu\BotMarfu.dll" "dotnet artefacts\opponent_v2\BotMarfu_old.dll" -t
